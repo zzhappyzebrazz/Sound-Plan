67757ddb5922fc30c91b92d2f3fbee3886abeddb
zzhappyzebrazz <b8.bbl.lmh@gmail.com>
2022-12-21 21:29:41 +0700

    page navigator

--- sound_plan/player/urls.py
+++ sound_plan/player/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from django.urls import path, re_path
 from player.views import *
 
-app_name = 'store'
+app_name = 'player'
 
 urlpatterns = [
     path('', index, name="index"),
     path('albums-store', albums_store, name="albums-store"),
     path('blog', blog, name="blog"),
     path('contact', contact, name="contact"),
-    path('elements', elements, name="element"),
+    path('elements', elements, name="elements"),
     path('event', event, name="event"),
     path('login', login, name="login"),
 
