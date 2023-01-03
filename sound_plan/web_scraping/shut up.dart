a60b53a38b908a958afff8181e907db66f35d7c2
Lại Minh Hà <b8.bbl.lmh@gmail.com>
2023-01-03 22:15:41 +0700

    manage working tree

---
+++ sound_plan/web_scraping/shut up.dart
@@ -0,0 +1,110 @@
+shut up
+/track/12sliObzVzbLbAlDb8eChf
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+34+35
+/track/1hG4V53eR16jg7jVTNLOiX
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+motive (with Doja Cat)
+/track/5VipERQ1ofCowecoFg2MVU
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+Doja Cat
+/artist/5cj0lLjcoR7YOSnhnX0Po5
+just like magic
+/track/1N9hFgcgWYbGINUKjhvcK6
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+off the table (with The Weeknd)
+/track/1qKdid2S9fZdSrzLaCcjcF
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+The Weeknd
+/artist/1Xyo4u8uXC1ZmMpatF05PJ
+six thirty
+/track/4rJWTThj9EWR6UqD1eVyge
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+safety net (feat. Ty Dolla $ign)
+/track/53Z8fbdVNus9Kiu7tWIBEC
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+Ty Dolla $ign
+/artist/7c0XG5cIJTrrAgEC3ULPiq
+my hair
+/track/5Ds3yH73nMHgkmJxcxqPrr
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+nasty
+/track/7wcl0YJBQ0ygTUUaXLRM1g
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+west side
+/track/7cFYqReKEigTxTm0bNE6YA
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+love language
+/track/4iIrJ94pkIEnGZWv1MhIRC
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+positions
+/track/3DFnLXa69NVlOjbhTbXXNn
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+obvious
+/track/65OEcDb4M3mAvEpSdXi6Lv
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+pov
+/track/3RP1eXaK8f4rwqY9fRKVRF
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+someone like u - interlude
+/track/3Os1431WclqyKxWqMzRESE
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+test drive
+/track/3eZYOQO4UzKrUDYDghtnFw
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+34+35 Remix (feat. Doja Cat, Megan Thee Stallion) - Remix
+/track/2sQH02O2HBCPJCztBrCWNa
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+Doja Cat
+/artist/5cj0lLjcoR7YOSnhnX0Po5
+Megan Thee Stallion
+/artist/181bsRPaVXVlUKXrxwZfHK
+worst behavior
+/track/6KIKaWKE9wV6mLjho3w61y
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+main thing
+/track/63WsFFnQ8CL941iZBELYsX
+Ariana Grande
+/artist/66CXWjxzNUsdJxJ2JdwvnR
+
+{
+    'artist': {
+        'name': 'Adele', 
+        'avatar': 'https://i.scdn.co/image/ab6761610000e5eb68f6e5892075d7f22615bd17'
+        }, 
+    'albums': {
+        '30': ['https://open.spotify.com/album/21jF5jlMtzo94wbxmJ18aa', 'https://i.scdn.co/image/ab67616d00001e02c6b577e4c4a6d326354a89f7', 'Album • 2021']
+        }, 
+    'song': [
+        ['Strangers By Nature', 'https://open.spotify.com/track/13CVSGLSFl4UxpDVR6u3dq', 'Adele'], 
+        ['Easy On Me', 'https://open.spotify.com/track/46IZ0fSY2mpAiktS3KOqds', 'Adele'], 
+        ['My Little Love', 'https://open.spotify.com/track/2DuPBbS5mIldXnh7Wum8Cy', 'Adele'], 
+        ['Cry Your Heart Out', 'https://open.spotify.com/track/09u787BYeYIGd2mFIJ505t', 'Adele'], 
+        ['Oh My God', 'https://open.spotify.com/track/3Kkjo3cT83cw09VJyrLNwX', 'Adele'], 
+        ['Can I Get It', 'https://open.spotify.com/track/6w8ZPYdnGajyfPddTWdthN', 'Adele'], 
+        ['I Drink Wine', 'https://open.spotify.com/track/6v0UJD4a2FtleHeSYVX02A', 'Adele'], 
+        ['All Night Parking (with Erroll Garner) Interlude', 'https://open.spotify.com/track/496vIKS2INz840xZaKncvR', 'Adele'], 
+        ['Woman Like Me', 'https://open.spotify.com/track/2c6xRsQBv7pynuhYhWoHxo', 'Adele'], 
+        ['Hold On', 'https://open.spotify.com/track/6bGMSP3H9YqkmaLnaJTIoF', 'Adele'], 
+        ['To Be Loved', 'https://open.spotify.com/track/10ImcQk9tihY1EKMDIbvXJ', 'Adele'], 
+        ['Love Is A Game', 'https://open.spotify.com/track/2j3GxEsbNYNeEzz86wDY4J', 'Adele']
+            ]
+}
